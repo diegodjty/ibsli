8b868fb0bb858c989d8d8fbd4809cfb084d22d21
Diego Taveras <diegotaveras2@outlook.com>
2020-11-14 18:03:45 -0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>app</h1>
+  );
+}
+
+export default App;
---
+++ src/img/index.js
@@ -0,0 +1,3 @@
+export { default as About } from './about.svg';
+export { default as Hero } from './hero.svg';
+export { default as LongIsland } from './LongIsland.svg';
