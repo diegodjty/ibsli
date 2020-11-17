d9f0978e32017f7e9f3799885e24106073b189c4
Diego Taveras <diegotaveras2@outlook.com>
2020-11-17 08:02:28 -0500

    Long island Treemap demo

--- src/pages/LongIsland/LiExport.js
+++ src/pages/LongIsland/LiExport.js
@@ -1,61 +1,105 @@
-import React,{useState} from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 import styled from '@emotion/styled';
-import { LiMap } from '../../img';
-import Slider from '@material-ui/core/Slider';
+import Chart from "react-apexcharts"
+import data2014 from './data2014.json'
+import data2015 from './data2015.json'
+import data2016 from './data2016.json'
+
+const newData = [];
+const nassau = [];
+const suffolk = [];
+
+
+const modifydata = (data)=>{
+  data.map((industry)=>{
+    newData.push({
+      county: industry.COUNTY,
+      x: industry.DESCRIPTION,
+      y: industry.VALUE.replace(/,/g,'')
+    })
+  })
+
+  newData.map((industry,index)=>{
+    if(industry.county==="Nassau County"){
+      console.log(industry.y)
+      let onlyNumbers = parseInt(industry.y)
+      if(!isNaN(onlyNumbers)){
+       const num = Math.round(onlyNumbers / 100000) / 10;
+       if(num!=0){
+         nassau.push({
+           x: industry.x,
+           y: num
+          })
+       }
+      }
+      
+    }else{
+      suffolk.push(industry)
+    }
+  })
+
+}
 
 const Container = styled.div`
-    .slider{
-        width: 50%;
+    max-width: 1424px;
+    margin: auto;
+    .chartContainer{
+      width: 50%;
+      margin: auto;
     }
 `;
 
 const LiExport = () => {
-
-    const [year,setYear] = useState('2014');
-
-    const marks = [
+    const [year,setYear] = useState(data2014)
+    
+    const handleChange = (e) =>{
+      setYear([e.target.value])
+    }
+    modifydata(year)
+    const [chart,setChart] = useState({
+      series: [
         {
-          value: 0,
-          label: '2014',
+          data: nassau
+        }
+      ],
+      options:{
+        chart: {
+          type: 'treemap'
         },
-        {
-          value: 25,
-          label: '2015',
+        title: {
+          text: 'Nassau Exports 2014'
         },
-        {
-          value: 50,
-          label: '2016',
-        },
-        {
-          value: 75,
-          label: '2017',
-        },
-        {
-          value: 100,
-          label: '2018',
-        },
-      ];
-      
-      function valuetext(value) {
-        return `${value}°C`;
+        plotOptions: {
+          treemap: {
+            distributed: true,
+            enableShades: false
+          }
+        }
       }
-      
-      function valueLabelFormat(value) {
-        return marks.findIndex((mark) => mark.value === value) + 1;
-      }
+    })
     return (
         <Container>
-            <img src={LiMap} alt=""/>
-            <Slider
-                defaultValue={0}
-                valueLabelFormat={valueLabelFormat}
-                getAriaValueText={valuetext}
-                aria-labelledby="discrete-slider-restrict"
-                step={null}
-                marks={marks}
-                className="slider"
-                onChange={(e)=>console.log(e.target.value)}
-      />
+          {/* <div className="year-container">
+            <label htmlFor="year">Select a Year:</label> 
+            <select name="" id="year" onChange={handleChange}>
+                <option value="data2014">2014</option>
+                <option value={`${data2015}`}>2015</option>
+                <option value={`${data2016}`}>2016</option>
+                <option value="2017">2017</option>
+                <option value="2018">2018</option>
+                <option value="2019">2019</option>
+            </select>
+        </div> */}
+        <div className="chartContainer">
+            <Chart
+              className='diego'
+              options={chart.options}
+              series={chart.series}
+              type="treemap"
+              height='700px'
+              width='100%'
+            />      
+        </div>
         </Container>
     );
 };
